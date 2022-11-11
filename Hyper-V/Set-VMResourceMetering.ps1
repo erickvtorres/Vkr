7dd77d5e6d3afa128ec789f76e0da809d4cb30e1
Erick Torres do Vale <41164630+erickvtorres@users.noreply.github.com>
2022-11-11 09:47:21 -0300

    Update Set-VMResourceMetering.ps1
    Fix bugs and improvements

--- Hyper-V/Set-VMResourceMetering.ps1
+++ Hyper-V/Set-VMResourceMetering.ps1
@@ -1,158 +1,176 @@
 <#
-    .SYNOPSIS
-    Gather VMs Resource Metering information, enable or disable.
+.SYNOPSIS
+    Gather VMs Resource Metering information, enable, disable or reset.
 
-    .LINK
-    Linkedin:      https://www.linkedin.com/in/erickvtorres/
-    GitHub:        https://github.com/erickvtorres/Vkr
+.DESCRIPTION
+    Resource Metering gets the hardware average usage from Virtual Machines.
+    Important - VMs needs to be configured with Dynamic Memory.
 
-    .NOTES
-    Date:          2022-02-03
-    LastUpdate:    2022-09-23
-    Version:       1.0
+.LINK
+    Linkedin    : https://www.linkedin.com/in/erickvtorres/
+    GitHub      : https://github.com/erickvtorres
 
-    .EXAMPLE
-    Set-VMResourceMetering -Status    # Get Resource Metering from VMs
-    Set-VMResourceMetering -Enable    # Enable Resource Metering
-    Set-VMResourceMetering -Disable   # Disable Resource Metering
+.NOTES
+    Creator     : Erick Torres do Vale
+    Contact     : ericktorres@hotmail.com.br
+    Date        : 2022-02-03
+    LastUpdate  : 2022-11-11
+    Version     : 1.1
+
+.EXAMPLE
+    Set-VMResourceMetering -ComputerName <server> -HypervModes <FailoverClustering|StandaloneServer> -Status
+    Set-VMResourceMetering -ComputerName <server> -HypervModes <FailoverClustering|StandaloneServer> -Action <Enable|Disable|Reset>
 #>
 
 function Set-VMResourceMetering {
-    [CmdletBinding(DefaultParameterSetName = 'Status')]
+    [CmdletBinding(DefaultParameterSetName = 'ComputerName')]
     param (
-        [Parameter(Mandatory = $true, Position = 0, ParameterSetName = 'Status')]
-        [switch] $Status,
+        [parameter(
+            Mandatory = $true,
+            Position = 0,
+            ParameterSetName = 'ComputerName'
+        )]
+        [string]
+        $ComputerName,
 
-        [Parameter(Mandatory = $true, Position = 0, ParameterSetName = 'Enable')]
-        [switch] $Enable,
+        [Parameter(
+            Mandatory = $true,
+            Position = 1
+        )]
+        [ValidateSet('FailoverClustering', 'StandaloneServer')]
+        [string]
+        $HypervMode,
 
-        [Parameter(Mandatory = $true, Position = 0, ParameterSetName = 'Disable')]
-        [switch] $Disable
+        [Parameter(
+            Mandatory = $false,
+            Position = 2
+        )]
+        [switch]
+        $Status,
+
+        [Parameter(
+            Mandatory = $false,
+            Position = 2
+        )]
+        [ValidateSet('Enable', 'Disable', 'Reset')]
+        [string]
+        $Action
     )
     
     begin {
-        $ClusterName = Read-Host "Hyper-V Cluster Name"
-        $Nodes = (Get-ClusterNode -Cluster $ClusterName).Name
-        $VirtualMachines = @()
-        $VirtualMachinesHashTable = @()
-        $Export = "C:\Reports"
+        $VMs = New-Object -TypeName System.Collections.ArrayList
+        $excptmsg = "$(Get-Date -Format dd-MM-yyyy),$($_.VMName),$($_.Node),$($_.Exception.Message)"
 
-        if (-Not (Test-Path $Export)) {
-            Write-Host "$([char]0x203A) Creating $($Export): " -NoNewline
-            try {
-                New-Item -ItemType Directory -Path $Export -Force | Out-Null
-                Write-Host "Done" -ForegroundColor Green
+        switch ($PSBoundParameters.ContainsKey('HypervMode')) {
+            FailoverClustering { 
+                try {
+                    Get-Cluster -Name $ComputerName -ErrorAction Stop | Out-Null
+                    $Nodes = (Get-ClusterNode -Cluster $ComputerName).Name
+                    Write-Verbose -Message "HypervMode: Failover Clustering, gathering nodes"
+                }
+                catch {
+                    Write-Warning $_.Exception.Message
+                }
             }
-            catch {
-                Write-Host $_.Exception.Message -ForegroundColor Red
+            StandaloneServer { 
+                try {
+                    $Nodes = (Get-VMHost -ComputerName $ComputerName -ErrorAction Stop).Name
+                    Write-Verbose -Message "HypervMode: Standalone Server"
+                }
+                catch {
+                    Write-Warning $_.Exception.Message
+                }
             }
+            Default { break }
         }
     }
     
     process {
-        
-        Write-Host "$([char]0x203A) Gathering VMs: " -NoNewline
-        foreach ($Node in $Nodes) {
-            try {
-                $VirtualMachines += Get-VM -ComputerName $Node
-            }
-            catch {
-                Write-Host $_.Exception.Message
-                Break
-            }
-        } #Gathering VMs
-
-        Write-Host "$($VirtualMachines.Count) found" -ForegroundColor Green
-        $VirtualMachines = $VirtualMachines | Sort-Object ComputerName,VMName
-
-        if ($Status) {
-            $Counter = 1
-            foreach ($VMItem in $VirtualMachines) {
-                Write-Progress -Activity "Gathering VMs details from Node $($VMItem.ComputerName)" -Status "Step $($Counter) of $($VirtualMachines.Count) complete"`
-                    -CurrentOperation "Current operation: $($VMItem.VMName)" -PercentComplete (($Counter / $VirtualMachines.Count) * 100)
-                $Counter++
-
-                $VirtualMachinesHashTable += [PSCustomObject]@{
-                    VMName   = $VMItem.VMName
-                    VMId     = $VMItem.VMId
-                    Metering = $VMItem.ResourceMeteringEnabled
-                    Uptime   = $VMItem.Uptime
-                    Status   = $VMItem.Status
-                    State    = $VMItem.State
-                    Node     = $VMItem.ComputerName
-                }
-            } #Hastable
-            Write-Progress -Activity "Completed" -Completed
-
-            $StatusFileName = "$($Export)\VMsResourceMetering-$(Get-Date -Format dd-MM-yyyy).xlsx"
-            Write-Host "$([char]0x203A) Exporting to $($StatusFileName)"
-            $VirtualMachinesHashTable | Export-Excel $StatusFileName -Append -WorksheetName 'Status' -AutoSize -FreezeTopRow -TableStyle Medium13
-        }
-
-        if ($Enable) {
-            $Counter = 1
-            $Success = 0
-            $Fail    = 0
-            foreach ($VMItem in $VirtualMachines) {
-                Write-Progress -Activity "Enabling VMs Resource Metering from Node $($VMItem.ComputerName)" -Status "Step $($Counter) of $($VirtualMachines.Count) complete"`
-                -CurrentOperation "Current operation: $($VMItem.VMName)" -PercentComplete (($Counter / $VirtualMachines.Count) * 100)
-                $Counter++
-
-                try {
-                    Enable-VMResourceMetering -VMName $VMItem.VMName -ComputerName $VMItem.ComputerName
-                    Reset-VMResourceMetering -VMName $VMItem.VMName -ComputerName $VMItem.ComputerName
-                    $Success++
-                }
-                catch {
-                    Write-Warning "$([char]0x203A) $($VMItem.VMName) - $($_.Exception.Message)"
-                    $Erro = "$(Get-Date -Format dd-MM-yyyy),$($VMItem.VMName),$($VMItem.ComputerName),$($_.Exception.Message)"
-                    $Erro | Out-File "$Export\Enable-VMResourceMetering-Log.log" -Append
-                    $Fail++
+        if ($PSBoundParameters.ContainsKey('Status')) {
+            Write-Verbose -Message "Status: Gathering status from VMs"
+            $Nodes | ForEach-Object {
+                Get-VM -ComputerName $ComputerName | ForEach-Object {
+                    [pscustomobject]@{
+                        VMName     = $_.VMName
+                        Node       = $_.ComputerName
+                        Metering   = $_.ResourceMeteringEnabled
+                        RAMDynamic = $_.DynamicMemoryEnabled
+                        Uptime     = $_.Uptime
+                        Status     = $_.Status
+                        State      = $_.State
+                    }
                 }
             }
         }
 
-        if ($Disable) {
-            $Counter = 1
-            $Success = 0
-            $Fail    = 0
-            foreach ($VMItem in $VirtualMachines) {
-                Write-Progress -Activity "Disabling VMs Resource Metering from Node $($VMItem.ComputerName)" -Status "Step $($Counter) of $($VirtualMachines.Count) complete"`
-                -CurrentOperation "Current operation: $($VMItem.VMName)" -PercentComplete (($Counter / $VirtualMachines.Count) * 100)
-                $Counter++
-
-                try {
-                    Disable-VMResourceMetering -VMName $VMItem.VMName -ComputerName $VMItem.ComputerName
-                    Reset-VMResourceMetering -VMName $VMItem.VMName -ComputerName $VMItem.ComputerName
-                    $Success++
+        if ($PSBoundParameters.ContainsKey('Action')) {
+            $Nodes | ForEach-Object {
+                Get-VM -ComputerName $ComputerName | ForEach-Object {
+                    $item = [pscustomobject]@{
+                        VMName   = $_.VMName
+                        Node     = $_.ComputerName
+                        Metering = $_.ResourceMeteringEnabled
+                        Uptime   = $_.Uptime
+                        Status   = $_.Status
+                        State    = $_.State
+                    }
+                    [void]$VMs.Add($item)
                 }
-                catch {
-                    Write-Warning "$($VMItem.VMName) - $($_.Exception.Message)"
-                    $Erro = "$(Get-Date -Format dd-MM-yyyy),$($VMItem.VMName),$($VMItem.ComputerName),$($_.Exception.Message)"
-                    $Erro | Out-File "$Export\Disable-VMResourceMetering-Log.log" -Append
-                    $Fail++
+            }
+
+            $Fail = 0
+            $VMs = $VMs | Sort-Object Node,VMName
+            switch ($Action) {
+                Enable {
+                    $VMs | ForEach-Object{
+                        try {
+                            Enable-VMResourceMetering -ComputerName $_.Node -VMName $_.VMName
+                            Write-Verbose -Message "Running action 'Enable' at $($_.VMName) on $($_.Node)"
+                        }
+                        catch {
+                            Write-Warning "$([char]0x203A) $($_.VMName) - $($_.Exception.Message)"
+                            $excptmsg | Out-File "$env:TEMP\Set-VMResourceMetering-Log.log" -Append
+                            $Fail++
+                        }
+                    }
+                }
+                Disable { 
+                    $VMs | ForEach-Object{
+                        try {
+                            Disable-VMResourceMetering -ComputerName $_.Node -VMName $_.VMName
+                            Write-Verbose -Message "Running action 'Disable' at $($_.VMName) on $($_.Node)"
+                        }
+                        catch {
+                            Write-Warning "$([char]0x203A) $($_.VMName) - $($_.Exception.Message)"
+                            $excptmsg | Out-File "$env:TEMP\Set-VMResourceMetering-Log.log" -Append
+                            $Fail++
+                        }
+                    }
+                }
+                Reset {
+                    $VMs | ForEach-Object{
+                        try {
+                            Reset-VMResourceMetering -ComputerName $_.Node -VMName $_.VMName
+                            Write-Verbose -Message "Running action 'Reset' at $($_.VMName) on $($_.Node)"
+                        }
+                        catch {
+                            Write-Warning "$([char]0x203A) $($_.VMName) - $($_.Exception.Message)"
+                            $excptmsg | Out-File "$env:TEMP\Set-VMResourceMetering-Log.log" -Append
+                            $Fail++
+                        }
+                    }
+                }
+                Default { 
+                    Break
                 }
             }
         }
     }
     
     end {
-        if ($Status){
-            Invoke-Expression "explorer '/select,$StatusFileName'"
-        }
-
-        if ($Enable){
-            Write-Host "$([char]0x203A) Enabled Resource Metering for $($VirtualMachines.Count) virtual machines from Cluser $($ClusterName)"
-            if ($Fail -ge 1){
-                Write-Host "$([char]0x203A) $($Fail) virtual machines has not enabled, see log - $Export\Enable-VMResourceMetering-Log.log" -ForegroundColor Red
-            }
-        }
-
-        if ($Disable){
-            Write-Host "$([char]0x203A) Disabled Resource Metering for $($VirtualMachines.Count) virtual machines from Cluser $($ClusterName)"
-            if ($Fail -ge 1){
-                Write-Host "$([char]0x203A) $($Fail) virtual machines has not disabled, see log $Export\Enable-VMResourceMetering-Log.log" -ForegroundColor Red
-            }
+        if ($Fail -gt 0) {
+            Write-Output "Finished with $Fail errors | Action runned at $($VMs.Count)"
+            Write-Output "See log at $env:TEMP\Set-VMResourceMetering-Log.log"
         }
     }
 }
